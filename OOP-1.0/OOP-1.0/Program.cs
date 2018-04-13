8e2b5fe24922a2347376c6c4dc3f01b4c4a8ce39
Иван Волков <ivan.volkov.rzn@gmail.com>
2018-04-13 11:49:42 +0300

    Задание по OOP готово. можно проверять

--- OOP-1.0/OOP-1.0/Program.cs
+++ OOP-1.0/OOP-1.0/Program.cs
@@ -6,21 +6,34 @@ using System.Threading.Tasks;
 
 namespace OOP_1._0
 {
-   static  class Program
+     class Program
     {
-       
+       static OOP_1._0.grandchildren.Warhammer40K war = new OOP_1._0.grandchildren.Warhammer40K();
+        static OOP_1._0.grandchildren.Monopoly mon = new OOP_1._0.grandchildren.Monopoly();
+        static OOP_1._0.grandchildren.Poker pok = new OOP_1._0.grandchildren.Poker();
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
 
-            OOP_1._0.grandchildren.Warhammer40K war = new OOP_1._0.grandchildren.Warhammer40K();
-            OOP_1._0.grandchildren.Monopoly mon = new OOP_1._0.grandchildren.Monopoly();
-            OOP_1._0.grandchildren.Poker pok = new OOP_1._0.grandchildren.Poker();
 
+
+            Object();
+
+        }
+
+
+        protected static void Object()
+        {
             war.Rules();
             mon.Cards();
             pok.fishki();
-
         }
+
+
+
+
+
+
+
     }
 }
